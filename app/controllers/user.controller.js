4f7608cb3a6c2c5a6e05e169aeae3f3aa5acc013
simba830 <superdevpro123@gmail.com>
2023-08-21 15:35:14 -0400

    updage code

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,5 +1,5 @@
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const db = require("../models");
 const User = db.user;
 
@@ -8,10 +8,18 @@ exports.register = async (req, res) => {
   try {
     const { email, password } = req.body;
 
+    // Check if user's email exists in the email list
+    const emailList = ["test@test.com", "admin@admin.com"]; // Replace with your pre-defined email list
+    if (!emailList.includes(email)) {
+      return res
+        .status(400)
+        .json({ error: "Email not found in the email list" });
+    }
+
     // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
-      return res.status(400).json({ error: 'Email already exists' });
+      return res.status(400).json({ error: "Email already registered" });
     }
 
     // Hash the password
@@ -21,9 +29,9 @@ exports.register = async (req, res) => {
     const newUser = new User({ email, password: hashedPassword });
     await newUser.save();
 
-    res.status(201).json({ message: 'User registered successfully' });
+    res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
   }
 };
 
@@ -35,20 +43,52 @@ exports.login = async (req, res) => {
     // Find the user by email
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+      return res.status(401).json({ error: "Invalid email or password" });
     }
 
     // Compare the password
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(401).json({ error: 'Invalid email or password' });
+      return res.status(401).json({ error: "Invalid email or password" });
     }
 
     // Create and sign a JWT token
-    const token = jwt.sign({ userId: user._id }, '123');
+    const token = jwt.sign({ userId: user._id }, "123");
 
     res.json({ token });
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+exports.changePassword = async (req, res) => {
+  try {
+    const { userId, currentPassword, newPassword } = req.body;
+
+    // Find the user by userId
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // Compare the current password
+    const isCurrentPasswordValid = await bcrypt.compare(
+      currentPassword,
+      user.password
+    );
+    if (!isCurrentPasswordValid) {
+      return res.status(401).json({ error: "Invalid current password" });
+    }
+
+    // Hash the new password
+    const hashedNewPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update the user's password
+    user.password = hashedNewPassword;
+    await user.save();
+
+    res.json({ message: "Password changed successfully" });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
   }
 };
